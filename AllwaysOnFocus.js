3466c920ede491b8983133033cf6b11038b53caa
Adriel Lins Maciel <adriel.maciel@compasso.com.br>
2018-07-17 15:13:47 -0300

    RollBack one file for script

--- AllwaysOnFocus.js
+++ AllwaysOnFocus.js
@@ -1,3 +1,14 @@
+// ==UserScript==
+// @name     		AlwaysFocus
+// @version      	1.0
+// @description  	JavaScript Extension Td Interativa
+// @author       	Adriel Lins Maciel <adriellm@gmail.com>
+// @source   		https://github.com/AdrielLins/create-environment/blob/master/AllwaysOnFocus.js
+// @match        	http://tdinterativa.natura.net/*
+// @run-at   		document-start
+// @grant    		none
+// ==/UserScript==
+
 document.hasFocus = function () {
     return true;
 };
