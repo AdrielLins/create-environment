2ed5e9095ed54acf224b329b68184d8349944fc9
Adriel Lins Maciel <adriel.maciel@compasso.com.br>
2018-07-17 15:08:24 -0300

    Split files into usear and function

--- AllwaysOnFocus.js
+++ AllwaysOnFocus.js
@@ -1,14 +1,3 @@
-// ==UserScript==
-// @name     		AlwaysFocus
-// @version      	1.0
-// @description  	JavaScript Extension Td Interativa
-// @author       	Adriel Lins Maciel <adriellm@gmail.com>
-// @source   		https://github.com/AdrielLins/create-environment/blob/master/AllwaysOnFocus.js
-// @match        	http://tdinterativa.natura.net/*
-// @run-at   		document-start
-// @grant    		none
-// ==/UserScript==
-
 document.hasFocus = function () {
     return true;
 };
---
+++ user.AlwaysOnFocus.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name     		AlwaysFocus
+// @version      	1.0
+// @description  	JavaScript Extension Td Interativa
+// @author       	Adriel Lins Maciel <adriellm@gmail.com>
+// @match        	http://tdinterativa.natura.net/*
+// @require      	https://raw.githubusercontent.com/AdrielLins/create-environment/blob/master/AllwaysOnFocus.js
+// @run-at   		document-start
+// @grant    		none
+// ==/UserScript==
